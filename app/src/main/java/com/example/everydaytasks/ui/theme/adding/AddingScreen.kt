02e801341e32db1506995731869c7272c015289c
DTIXD <abdua3298@gmail.com>
2025-07-05 09:48:48 -0400

    Adding List of Every Day Tasks in Adding Screen

--- app/src/main/java/com/example/everydaytasks/ui/theme/adding/AddingScreen.kt
+++ app/src/main/java/com/example/everydaytasks/ui/theme/adding/AddingScreen.kt
@@ -10,13 +10,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -33,6 +40,7 @@ import com.google.firebase.ktx.Firebase
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import com.example.everydaytasks.ui.theme.AlmostWhite
 import com.example.everydaytasks.ui.theme.Biruz
 import com.example.everydaytasks.ui.theme.DarkerThanAlmostWhite
@@ -74,81 +82,136 @@ fun AddingPage(
             .fillMaxSize(),
         contentScale = ContentScale.Crop
     )
-
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 15.dp),
-        contentAlignment = Alignment.BottomCenter
+            .padding(top = 25.dp)
     ) {
         Box(
             modifier = Modifier
-                .fillMaxWidth(.95f)
-                .padding(bottom = 25.dp)
-                .offset(y = (-70).dp)
-                .background(
-                    color = AlmostWhite,
-                    shape = RoundedCornerShape(10.dp)
-                )
+                .fillMaxWidth()
+                .fillMaxHeight(.6f)
         ) {
-            Column(
+            LazyColumn(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(15.dp)
+                    .fillMaxSize()
             ) {
-                Text(
-                    text = "ADD TASK",
-                    color = Biruz
-                )
-                Spacer(
-                    modifier = Modifier
-                        .height(20.dp)
-                )
-                TextField(
-                    value = newTask.value,
-                    onValueChange = {
-                        newTask.value = it
-                    },
-                    shape = RoundedCornerShape(7.dp),
-                    colors = TextFieldDefaults.colors(
-                        unfocusedContainerColor = DarkerThanAlmostWhite,
-                        focusedContainerColor = DarkerThanAlmostWhite,
-                        unfocusedIndicatorColor = Color.Transparent,
-                        focusedIndicatorColor = Color.Transparent,
-                    ),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(50.dp)
-                )
-                Spacer(
-                    modifier = Modifier
-                        .height(17.dp)
-                )
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                ) {
-                    Image(
-                        painterResource(id = R.drawable.addbutton),
-                        contentDescription = null,
-                        Modifier
-                            .padding(5.dp)
-                            .clickable {
-                                onNavigationToProgressPage(
-                                    ProgressScreenDataObject(
-                                        newTask = newTask.value,
-                                        isCompleted = false,
-                                        key = key,
-                                        dayAdded = today.toString(),
-                                        daySelected = today.toString(),
-                                        category = if (addForToday.value) "Today"
-                                        else "EveryDay",
-                                        wasAdded = addForToday.value,
-                                        lastAdded = today.toString()
-                                    )
+                items(list.value) { progressScreenDataObject ->
+                    val isExtended = remember {
+                        mutableStateOf(false)
+                    }
+
+                    if (
+                        progressScreenDataObject.category
+                        == "EveryDay"
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                        ) {
+                            Row(
+                                modifier = Modifier
+                                    .padding(10.dp)
+                            ) {
+                                Text(
+                                    text = progressScreenDataObject.newTask,
+                                    modifier = Modifier
+                                        .padding(
+                                            top = 10.dp,
+                                            end = 40.dp
+                                        )
+                                        .clickable {
+                                            isExtended.value = !isExtended.value
+                                        },
+                                    fontSize = 20.sp,
+                                    maxLines =
+                                        if (isExtended.value) 100
+                                        else 1
                                 )
-                                fs.collection("tasks")
-                                    .document(key).set(
+                            }
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.End
+                            ) {
+                                IconButton(
+                                    onClick = {
+                                        fs.collection("tasks")
+                                            .document(progressScreenDataObject.key)
+                                            .delete()
+                                    }
+                                ) {
+                                    Icon(
+                                        Icons.Default.Delete,
+                                        contentDescription = ""
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 15.dp),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(.95f)
+                    .padding(bottom = 25.dp)
+                    .offset(y = (-70).dp)
+                    .background(
+                        color = AlmostWhite,
+                        shape = RoundedCornerShape(10.dp)
+                    )
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(15.dp)
+                ) {
+                    Text(
+                        text = "ADD TASK",
+                        color = Biruz
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .height(20.dp)
+                    )
+                    TextField(
+                        value = newTask.value,
+                        onValueChange = {
+                            newTask.value = it
+                        },
+                        shape = RoundedCornerShape(7.dp),
+                        colors = TextFieldDefaults.colors(
+                            unfocusedContainerColor = DarkerThanAlmostWhite,
+                            focusedContainerColor = DarkerThanAlmostWhite,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Transparent,
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .height(17.dp)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Image(
+                            painterResource(id = R.drawable.addbutton),
+                            contentDescription = null,
+                            Modifier
+                                .padding(5.dp)
+                                .clickable {
+                                    onNavigationToProgressPage(
                                         ProgressScreenDataObject(
                                             newTask = newTask.value,
                                             isCompleted = false,
@@ -161,36 +224,51 @@ fun AddingPage(
                                             lastAdded = today.toString()
                                         )
                                     )
-                            },
-                        contentScale = ContentScale.Crop
-                    )
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.End
-                    ) {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally
+                                    fs.collection("tasks")
+                                        .document(key).set(
+                                            ProgressScreenDataObject(
+                                                newTask = newTask.value,
+                                                isCompleted = false,
+                                                key = key,
+                                                dayAdded = today.toString(),
+                                                daySelected = today.toString(),
+                                                category = if (addForToday.value) "Today"
+                                                else "EveryDay",
+                                                wasAdded = addForToday.value,
+                                                lastAdded = today.toString()
+                                            )
+                                        )
+                                },
+                            contentScale = ContentScale.Crop
+                        )
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.End
                         ) {
-                            Image(
-                                painterResource(
-                                    id =
-                                        if (addForToday.value) R.drawable.completed
-                                        else R.drawable.notcompleted
-                                ),
-                                contentDescription = null,
-                                Modifier
-                                    .padding(5.dp)
-                                    .size(30.dp)
-                                    .clickable {
-                                        addForToday.value = !addForToday.value
-                                    },
-                                contentScale = ContentScale.Crop
-                            )
-                            Text(
-                                text = "Add Just For Today",
-                                color = Biruz
-                            )
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Image(
+                                    painterResource(
+                                        id =
+                                            if (addForToday.value) R.drawable.completed
+                                            else R.drawable.notcompleted
+                                    ),
+                                    contentDescription = null,
+                                    Modifier
+                                        .padding(5.dp)
+                                        .size(30.dp)
+                                        .clickable {
+                                            addForToday.value = !addForToday.value
+                                        },
+                                    contentScale = ContentScale.Crop
+                                )
+                                Text(
+                                    text = "Add Just For Today",
+                                    color = Biruz
+                                )
+                            }
                         }
                     }
                 }
