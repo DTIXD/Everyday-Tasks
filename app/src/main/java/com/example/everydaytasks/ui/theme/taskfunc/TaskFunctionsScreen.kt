2437dbc7d533fbb836009524ac06e8ef7e632a9e
DTIXD <abdua3298@gmail.com>
2025-08-26 21:11:36 -0400

    Arrowback icon, title for text func

--- app/src/main/java/com/example/everydaytasks/ui/theme/taskfunc/TaskFunctionsScreen.kt
+++ app/src/main/java/com/example/everydaytasks/ui/theme/taskfunc/TaskFunctionsScreen.kt
@@ -2,10 +2,18 @@ package com.example.everydaytasks.ui.theme.taskfunc
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.everydaytasks.ui.theme.BottomMenuColor
 import com.example.everydaytasks.ui.theme.progress.ProgressScreenDataObject
@@ -22,9 +30,27 @@ fun TaskFunctionsPage(
                 color = BottomMenuColor
             )
     ) {
-        Text(
-            text = "Hello",
-            fontSize = 100.sp
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(
+                        horizontal = 15.dp
+                    )
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Add",
+                    tint = Color.White
+                )
+                Text(
+                    text = "Task Actions",
+                    fontSize = 25.sp,
+                    color = Color.White
+                )
+            }
+        }
     }
 }
